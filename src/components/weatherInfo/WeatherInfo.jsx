8974f5a0b03dd2ad01134dedc8f2df81eb61ea32
Namik Guseinov <gnamik007@gmail.com>
2023-02-19 16:30:52 +0300

    adding country flag title

--- src/components/weatherInfo/WeatherInfo.jsx
+++ src/components/weatherInfo/WeatherInfo.jsx
@@ -8,7 +8,7 @@ const WeatherInfo = ({ data }) => {
             {data.name} <img
                className={styles.countryFlag}
                src={`https://flagcdn.com/w40/${data.sys.country.toLowerCase()}.png`}
-               title={data.sys.country}
+               title={new Intl.DisplayNames(['ru'], { type: 'region' }).of(data.sys.country)}
                alt={data.sys.country} />
          </h2>
 
