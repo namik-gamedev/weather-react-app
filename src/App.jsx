692d23d4a6029d1ffc5c68da232f4d0a0e4475f3
Namik Guseinov <gnamik007@gmail.com>
2023-02-19 18:08:24 +0300

    tabs fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,41 @@ import DataFetching from './tools/DataFetching';
 import WeatherService from './API/WeatherService';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [dataNotFound, setDataNotFound] = useState(false)
-  const [weatherData, setWeatherData] = useState({})
-  const [isNight, setIsNight] = useState(false)
+   const [isLoading, setIsLoading] = useState(false)
+   const [dataNotFound, setDataNotFound] = useState(false)
+   const [weatherData, setWeatherData] = useState({})
+   const [isNight, setIsNight] = useState(false)
 
-  async function fetchData(location) {
-    if (location === '') {
-      return
-    }
+   async function fetchData(location) {
+      if (location === '') {
+         return
+      }
 
-    setIsLoading(true)
+      setIsLoading(true)
 
-    const response = await WeatherService.getAll(location)
-    const [data, notFound, isNight] = await DataFetching.fetchData(response)
-    setWeatherData(data)
-    setDataNotFound(notFound)
-    setIsNight(isNight)
+      const response = await WeatherService.getAll(location)
+      const [data, notFound, isNight] = await DataFetching.fetchData(response)
+      setWeatherData(data)
+      setDataNotFound(notFound)
+      setIsNight(isNight)
 
-    setIsLoading(false)
-  }
+      setIsLoading(false)
+   }
 
-  return (
-    <div className={isNight ? 'App night' : 'App'}>
-      <div className="container">
-        <SearchForm fetchData={fetchData} />
-        {isLoading
-          ? < Loader />
-          : Object.keys(weatherData).length > 0 &&
-          (dataNotFound
-            ? <NotFound /> :
-            < WeatherInfo data={weatherData} />)
-        }
+   return (
+      <div className={isNight ? 'App night' : 'App'}>
+         <div className="container">
+            <SearchForm fetchData={fetchData} />
+            {isLoading
+               ? < Loader />
+               : Object.keys(weatherData).length > 0 &&
+               (dataNotFound
+                  ? <NotFound /> :
+                  < WeatherInfo data={weatherData} />)
+            }
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
