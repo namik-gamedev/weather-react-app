8398b07e116476fc778c4a5f5d749789fc8471df
Namik Guseinov <gnamik007@gmail.com>
2023-02-19 12:24:47 +0300

    fix WeatherService method name

--- src/API/WeatherService.js
+++ src/API/WeatherService.js
@@ -1,5 +1,5 @@
 export default class WeatherService {
-   static async getData(location) {
+   static async getAll(location) {
       const APIKey = '728b0ee6df5687559812bd3169ad77b7'
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=ru&appid=${APIKey}`
       const response = await fetch(url)
